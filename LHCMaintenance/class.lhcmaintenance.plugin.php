fd76c1c22653a1bd47eeec9b61c5d092656a7c2e
YaelDD <melodicfingers@lifeheart.club>
2018-07-10 07:38:27 -0500

    Updated message showing method and therefore the style of the message

--- LHCMaintenance/class.lhcmaintenance.plugin.php
+++ LHCMaintenance/class.lhcmaintenance.plugin.php
@@ -9,7 +9,7 @@
 $PluginInfo['LHCMaintenance'] = array(
    'Name' => '[LHC]Maintenance',
    'Description' => 'Maintenance plugin for Vanilla 2, allows you to close your site to do maintenance. You can customize the image by replacing the current one (maintenance.png).',
-   'Version' => '1.0.1',
+   'Version' => '1.2.0',
    "mobileFriendly" => true,
    'Author' => "Yael Delgado",
    'AuthorUrl' => 'https://yaeldd.lifeheart.club',
@@ -17,30 +17,64 @@ $PluginInfo['LHCMaintenance'] = array(
 );
 
 class LHCMaintenancePlugin  extends Gdn_Plugin {
-
+    public function Base_Render_Before($Sender) {
+        if ( !Gdn::Session()->CheckPermission('Garden.Settings.Manage')) {
+		    // Without Permission
+		    echo '
+		    <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>'.$Sender->Title().' - Under Maintenance</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <style>
+                        body {
+                            background: #000;
+                        }
+                        #wrapper {
+                            width: 80%;
+                            margin: auto;
+                            margin-top: 50px;
+                            margin-bottom: 50px;
+                        }
+                        #header {
+                            color: #fff;
+                            font-family: monospace;
+                        }
+                        #content {
+                            background: #ffffff;
+                            text-align: center;
+                            padding: 50px;
+                        }
+                        #maintenance-text {
+                            display: block;
+                            margin: auto;
+                            font-family: monospace;
+                            font-size: 20px;
+                            font-weight: bold;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <div id="wrapper">
+                        <div id="header">
+                            <h1>'.$Sender->Title().' - Under Maintenance</h1>
+                        </div>
+                        <div id="content">
+                            <img src="./plugins/LHCMaintenance/maintenance.png" style="max-width:60%;" alt="Maintenance" />
+                            <span id="maintenance-text">'.$Sender->Title().' is currently under maintenance, please check back later!</span>
+                        </div>
+                    </div>
+                </body>
+		    ';
+		    exit();
+		}
+    }
+    
     //public function Base_Render_Before($Sender) {
     public function Base_Render_After($Sender) {
 		if ( Gdn::Session()->CheckPermission('Garden.Settings.Manage')) {
 		    // With Permission
 		    echo '<script type="text/javascript">$("body").prepend("<div style=\"color:yellow;background-color:red;text-align:center;padding:15px;\"><b>The site is currently in Maintenance Mode</b></div>");</script>';
-		} else {
-		    // Without Permission
-		    echo '
-    		    <style>
-                    .container {
-                        min-height: -webkit-fill-available;
-                    }
-                    #maintenance {
-                        zoom: 1;
-                        margin: 20px 0 20px 0;
-                        padding-bottom: 20px;
-                        text-align: center;
-                    }
-                </style>
-                <script type="text/javascript">
-                    $(\'<div id="maintenance"><img src="./plugins/LHCMaintenance/maintenance.png" style="max-width:25%;" alt="Maintenance"></div>\').replaceAll(\'#Body\');
-                </script>
-		    ';
 		}
     }
     
